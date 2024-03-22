936bf901ef6d3a38c30d82ebd599d9ad0dbec2c1
givoxxs <toanyogame@gmail.com>
2024-03-22 15:24:37 +0700

    btvn update delete

--- Lecture11/BTVN/assets/main.js
+++ Lecture11/BTVN/assets/main.js
@@ -19,7 +19,8 @@ let cardsContainer = document.getElementById("cards")
 let createBtn = document.getElementById("create-btn")
 let popupContainer = document.querySelector(".popup-container")
 let popupMain = document.querySelector(".popup-main")
-
+let onAction = document.querySelector(".clickPopup")
+let clickDelete = document.querySelector(".clickDelete")
 
 // open popup
 createBtn.addEventListener('click', function() {
@@ -36,17 +37,13 @@ popupContainer.addEventListener('click', function() {
 })
 
 // prevent main popup close
-
-function onCreate() {
-    console.log("Hello")
-}
-
 popupMain.addEventListener('click', function(event) {
-    console.log("event", event)
+    // console.log("event", event)
     event.stopPropagation()
 })
 
 function onCreate() {
+    // onAction.classList.add("active-edit-non")
     let name = document.getElementById("field-name").value
     let phone = document.getElementById("field-phone").value
     let email = document.getElementById("field-email").value
@@ -59,14 +56,60 @@ function onCreate() {
     localStorage.setItem("contacts", JSON.stringify(contacts))
     render()
 }
-
+// Delete Card
 function deleteCard(index) {
-    console.log(index, "Hello")
+    console.log(index, "Delete Card")
     contacts.splice(index, 1)
     render()
     localStorage.setItem("contacts", JSON.stringify(contacts))
 }
+// Edit Update card
+let editIndex = null
+// khi click Edit
+function editCard(index) {
+    editIndex = index
+    // console.log("EditIndex: ", editIndex) 
+    editShow(editIndex)
+}
+// Save card
+function saveCard() {
+    let name = document.getElementById("field-name").value
+    let phone = document.getElementById("field-phone").value
+    let email = document.getElementById("field-email").value
+    if (editIndex !== null) { 
+        contacts[editIndex].name = name
+        contacts[editIndex].phone = phone
+        contacts[editIndex].email = email
+        localStorage.setItem("contacts", JSON.stringify(contacts))
+        render()
+    } else {
+        console.error("Không thể chỉnh sửa vì không tìm thấy phần tử trong mảng contacts.");
+    }
+    oncancel()
+} 
+// Show card
+function editShow(editIndex) {
+    onAction.classList.add("active-edit")
+    clickDelete.classList.add("active-edit-non")
+    if (editIndex !== null) {
+        popupContainer.classList.toggle("active")
+        document.getElementById("field-name").value = contacts[editIndex].name
+        document.getElementById("field-phone").value = contacts[editIndex].phone
+        document.getElementById("field-email").value = contacts[editIndex].email
+    }
+}
 
+function oncancel() {
+    editIndex = null;
+    onAction.classList.remove("active-edit")
+    clickDelete.classList.remove("active-edit-non")
+    document.getElementById("field-name").value = ""
+    document.getElementById("field-phone").value = ""
+    document.getElementById("field-email").value = ""
+    popupContainer.classList.remove("active")
+}
+
+// Render
 function render() {
     let elements = contacts.map((item, index) => {
         return `
@@ -88,7 +131,7 @@ function render() {
                 </div>
 
                 <div class="action">
-                    <img id="edit" src="./assets/img/icon/edit.svg" alt="">
+                    <img id="edit" onclick=editCard(${index}) src="./assets/img/icon/edit.svg" alt="">
                     <img id="delete" onclick="deleteCard(${index})" src="./assets/img/icon/delete.svg" alt="">
                 </div>
             </div>
@@ -98,4 +141,6 @@ function render() {
     cardsContainer.innerHTML = elements.join('')
 }
 
-render(contacts)
+render()
+
+
