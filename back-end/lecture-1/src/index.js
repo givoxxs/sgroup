805fe13b01124556d82a191d93b9d09a864aece5
givoxxs <toanyogame@gmail.com>
2024-05-13 21:22:37 +0700

    update btvn init nodejs project and use postman

--- back-end/lecture-1/src/index.js
+++ back-end/lecture-1/src/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3000;
 
-const users = [
+app.use(bodyParser.json());
+
+let users = [
   { id: 1, 
     name: 'Phan Văn Toàn',
     age: 19
@@ -18,28 +21,63 @@ const users = [
   { id: 4,
     name: 'Quang Nguyễn',
     age: 21
+  },
+  {
+    id: 5,
+    name: 'Giáo Phan',
+    age: 100
   }
-]
+];
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
+// Middleware để kiểm tra token
+const checkToken = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (token === 'Bearer token') {
+    next();
+  } else {
+    res.status(401).json({ error: 'Unauthorized' });
+  }
+};
+// Get all users
+app.get('/users', checkToken, (req, res) => {
+  console.log('GET all users');
+  res.json(users);
+});
+// Get detail info user by id
+app.get('/users/:id', checkToken, (req, res) => {
+  const user = users.find(user => user.id === parseInt(req.params.id));
+  console.log(`GET user with id ${req.params.id}`);
+  res.json(user);
+});
+// post user
+app.post('/users', checkToken, (req, res) => {
+  const newUser = req.body;
+  console.log('POST new user:', newUser);
+  users.push(newUser);
+  res.json(newUser);
+});
+
+// Put user
+app.put('/users/:id', checkToken, (req, res) => {
+  const userId = parseInt(req.params.id);
+  const updatedUser = req.body;
+  console.log(`PUT user with id ${userId}:`, updatedUser);
+  const index = users.findIndex(user => user.id === userId);
+  if (index !== -1) {
+    users[index] = updatedUser;
+    res.json(updatedUser);
+  } else {
+    res.status(404).json({ error: 'User not found' });
+  }
+});
+// delete user
+app.delete('/users/:id', checkToken, (req, res) => {
+  const userId = parseInt(req.params.id);
+  console.log(`DELETE user with id ${userId}`);
+  users = users.filter(user => user.id !== userId);
+  res.send(users);
 });
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
-});
-
-app.get('/about', (req, res) => {
-  res.send('Ho thanh tien lỏ!');
-});
-
-app.get('/user/:id', (req, res) => {
-  const user = users.filter(user => user.id === parseInt(req.params.id));
-  res.json(user);
-});
-
-app.get('/user', (req, res) => {
-  const id = parseInt(req.query.id);
-  const user = users.find(user => user.id === id);
-  res.json(user);
 });
